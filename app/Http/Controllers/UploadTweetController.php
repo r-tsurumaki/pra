130fba06237c888a88d4adab2765fda4139d1886
rkusen <r-kuwada@usen-next.jp>
2022-08-26 14:02:17 +0900

    4

--- app/Http/Controllers/UploadTweetController.php
+++ app/Http/Controllers/UploadTweetController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class UploadTweetController extends Controller
 {
-    //
+    /*public function upload(Request $request)
+{
+$request->validate([
+'image' => 'required|max:1024|mimes:jpg,jpeg,png,gif'
+]);
+$file_path = $request->image->store('images', 'public');
+print("<img src='". asset("$file_path"). "' width='300'>");
+print("<a href='upload_form'>画像投稿フォームに戻る</a>");
+}*/
+public function index()
+{
+//$upload_images = ::all();
+return view('list_tweet');
 }
+}
+
+
+
+
